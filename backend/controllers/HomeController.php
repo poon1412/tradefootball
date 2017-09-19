0fc835b53c9d4e4a90bbd77f362553785d6857a0
MSI\Poon <poon1412@gmail.com>
2017-09-20 03:07:24 +0700

    Home controller

---
+++ backend/controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+namespace backend\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use common\models\LoginForm;
+
+class HomeController extends Controller
+{
+public function behaviors()
+    {
+        return [
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+      $this->layout = "@backend/themes/new/index";
+      return $this->render('index');
+    }
+}
---
+++ backend/themes/new/index.php
@@ -0,0 +1,49 @@
+<?php
+use backend\assets\AppAsset;
+use yii\helpers\Html;
+/* @var $this \yii\web\View */
+/* @var $content string */
+// dmstr\web\AdminLteAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Morphing Page Transition | Demo 3 | Codrops</title>
+		<meta name="description" content="A page transition where one SVG path is morphed into another" />
+		<meta name="keywords" content="page transition, svg, morph, path, javascript, web design, web dev" />
+		<meta name="author" content="Codrops" />
+
+  <!-- Site Properties -->
+  <title>Homepage - TradeFootball</title>
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+    <?php $this->registerCssFile ( "@web/assets/HomeCSS/css/swing.css" );
+    $this->registerCssFile ( "@web/assets/HomeCSS/css/style7.css" );
+    $this->registerCssFile ( "@web/assets/HomeCSS/css/sreach.css" );
+    $this->registerCssFile ( "@web/assets/HomeCSS/css/normalize.css" );
+    $this->registerCssFile ( "@web/assets/HomeCSS/css/demo.css" );
+    $this->registerCssFile ( "@web/assets/HomeCSS/css/btt.css" );
+    $this->registerJsFile('@web/assets/HomeCSS/css/swing.js',['depends' => [\yii\web\JqueryAsset::className()]]);
+    $this->registerJsFile('@web/assets/HomeCSS/css/sreach.js',['depends' => [\yii\web\JqueryAsset::className()]]);
+    $this->registerJsFile('@web/assets/HomeCSS/css/imagesloaded.pkgd.min.js',['depends' => [\yii\web\JqueryAsset::className()]]);
+    $this->registerJsFile('@web/assets/HomeCSS/css/demo3.js',['depends' => [\yii\web\JqueryAsset::className()]]);
+    $this->registerJsFile('@web/assets/HomeCSS/css/demo.js',['depends' => [\yii\web\JqueryAsset::className()]]);
+    $this->registerJsFile('@web/assets/HomeCSS/css/charming.min.js',['depends' => [\yii\web\JqueryAsset::className()]]);
+    $this->registerJsFile('@web/assets/HomeCSS/css/anime.min.js',['depends' => [\yii\web\JqueryAsset::className()]]); ?>
+    <script>document.documentElement.className = 'js';</script>
+</head>
+
+
+<body class="demo-3 loading">
+
+<?php $this->beginBody() ?>
+
+    <?= $content ?>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
