54168151a556715494c140c7cd306207e4d0d569
MSI\Poon <poon1412@gmail.com>
2017-09-20 02:02:39 +0700

    Home Error +DBMS

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -62,6 +62,9 @@ class SiteController extends Controller
     {
         return $this->render('index');
     }
+    public function actionHome(){
+      return $this->render('home');
+    }
 
     /**
      * Login action.
