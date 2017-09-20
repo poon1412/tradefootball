cf778e6e155e1206648b419885165372a90e95c2
DESKTOP-G495A6E\moonp <moonpingza1@gmail.com>
2017-09-20 20:37:58 +0700

    create site button

--- backend/views/home/site.php
+++ backend/views/home/site.php
@@ -10,8 +10,19 @@ $baseUrl = \Yii::getAlias('@web/assets');
 <div class="ui container">
 
     <a href="<?php echo "http://localhost/tradefootball/backend/web/home/site"; ?>"><img src="<?=$baseUrl."/logo/logo.png"?>" width=150 height=100 ALIGN="LEFT" alt=""></a>
-    <a href="<?php echo "http://localhost/tradefootball/backend/web/home/player"; ?>"><button class="ui button">Player</button></a>
-    <a href="<?php echo "http://localhost/tradefootball/backend/web/home/team"; ?>"><button class="ui button">Team</button></a>
-    <button class="ui button">Button</button>
-    <button class="ui button">Button</button>
+
+<div class="ui two column grid">
 </div>
+	<br></br> 
+	<br></br>  
+    <div class="ui right aligned grid">
+    	<div class="right floated left aligned seven wide column">
+    		
+      			<button class="ui inverted blue button">Player</button>
+      			<button class="ui inverted blue button">Team</button>
+      			<button class="ui inverted blue button">Index</button>
+      			<button class="ui inverted blue button">Button</button>
+    		</div>
+      
+    	</div>
+	</div>
