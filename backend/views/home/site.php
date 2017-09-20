b7a1eef56ac9e3034d3e72b4d605163b8d729daf
DESKTOP-G495A6E\moonp <moonpingza1@gmail.com>
2017-09-20 21:54:28 +0700

    edit button

--- backend/views/home/site.php
+++ backend/views/home/site.php
@@ -7,8 +7,10 @@ $this->registerCssFile ( "@web/assets/semantic/semantic.min.css" );
 $this->registerJsFile('@web/assets/semantic/semantic.min.js',['depends' => [\yii\web\JqueryAsset::className()]]);
 $baseUrl = \Yii::getAlias('@web/assets');
 ?>
+
 <div class="ui container">
 
+<br></br>
     <a href="<?php echo "http://localhost/tradefootball/backend/web/home/site"; ?>"><img src="<?=$baseUrl."/logo/logo.png"?>" width=150 height=100 ALIGN="LEFT" alt=""></a>
 
 <div class="ui two column grid">
@@ -16,13 +18,25 @@ $baseUrl = \Yii::getAlias('@web/assets');
 	<br></br> 
 	<br></br>  
     <div class="ui right aligned grid">
-    	<div class="right floated left aligned seven wide column">
-    		
-      			<button class="ui inverted blue button">Player</button>
-      			<button class="ui inverted blue button">Team</button>
-      			<button class="ui inverted blue button">Index</button>
-      			<button class="ui inverted blue button">Button</button>
+    	<div class="right floated left aligned three wide column">
+      			<button class="ui inverted orange button"><i class="icon Lock"></i>Login</button>
     		</div>
-      
     	</div>
 	</div>
+
+<div class="ui two column grid">
+</div>
+	<br></br> 
+	<br></br>  
+	<div class="ui right aligned grid">
+  		<div class="left floated right aligned four wide column">
+      			<button class="ui inverted orange button"><i class="icon user"></i>Player</button>
+				<button class="ui inverted orange button"><i class="icon Soccer"></i>Team</button>
+    		</div>
+    	</div>
+	</div>
+
+
+
+
+
