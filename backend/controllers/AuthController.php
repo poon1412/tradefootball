5ea70da2927419cdca018df808edaf99240dda05
MSI\Poon <poon1412@gmail.com>
2017-09-20 18:54:12 +0700

    AuthController

--- backend/controllers/AuthController.php
+++ backend/controllers/AuthController.php
@@ -46,55 +46,41 @@ class AuthController extends Controller
     }
     public function actionLogin()
     {
-      $this->layout = "@backend/themes/new/site";
-    	return $this->render('login');
+      $request = Yii::$app->request;
+      $baseUrl = \Yii::getAlias('@web');
+      $session = Yii::$app->session;
+      if($session->has('user'))
+      {
+        return $this->redirect($baseUrl."/");
+      }else {
+        return $this->render('login');
+      }
     }
     public function actionLoginaction()
     {
       //config
       $request = Yii::$app->request;
       $baseUrl = \Yii::getAlias('@web');
-      $email = $request->post('email',null);
+      $username = $request->post('username',null);
       $pass = $request->post('password',null);
-      $customer = Customer::findOne(['email'=>$email]);
-      if(isset($customer) && ( md5($pass) == $customer->password ))
+      $team = Team::findOne(['username'=>$username]);
+      if(isset($customer) && ( $team == $team->password ))
       {
-        echo "TRUE";
-      }
-      else {
-        echo "FALSE";
-      }
+          $session->set('user', $team);
+          $session->setFlash('success', " ยินดีต้อนรับเข้าสู่ระบบ");
+          return $this->redirect($baseUrl."/");
+        }
+        else {
+          $session->setFlash('danger', " ชื่อใช้หรือรหัสผ่านไม่ถูก กรุณาเข้าสู่ระบบใหม่อีกครั้ง");
+          return $this->redirect($baseUrl."/auth/login");
+        }
     }
-    public function actionRegistersave()
+    public function actionLogout()
     {
-      //config
-      $request = Yii::$app->request;
+      $session = Yii::$app->session;
       $baseUrl = \Yii::getAlias('@web');
-      //get id edit , not id -> new
-      $fname = $request->get('firstname',null);
-      $lname = $request->get('lastname',null);
-      $email = $request->get('email',null);
-      $phone = $request->get('phone',null);
-      $pass = $request->get('password',null);
-      $address = $request->get('address',null);
-      $customer = new Customer;
-      $customer->firstname= $fname;
-      $customer->lastname= $lname;
-      $customer->phone= $phone;
-      $customer->email= $email;
-      $customer->password=  md5($pass);
-      $customer->address= $address;
-      if($customer->save()){
-        echo "success";
-      }else {
-        echo "error";
-      }
-       return $this->redirect($baseUrl."/auth/login");
-      }
-      public function actionLoginsave()
-      {
-        //config
-        $request = Yii::$app->request;
-        $baseUrl = \Yii::getAlias('@web');
-      }
+      $session->remove('user');
+      return $this->redirect($baseUrl."/");
+    }
+
     }
